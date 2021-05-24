e2dae8974326cccb8a7bcedb6490e73b095ebbc6
guinotix <al375927@uji.es>
2021-05-24 22:02:05 +0200

    Validación del login. Usuario gestormunicipal1 como prueba. Todavía no va el html

---
+++ src/main/java/com/ams/ei1027espaciosnaturales/dao/FakeInternalUserProvider.java
@@ -0,0 +1,45 @@
+package com.ams.ei1027espaciosnaturales.dao;
+
+import com.ams.ei1027espaciosnaturales.model.UserInterno;
+import org.jasypt.util.password.BasicPasswordEncryptor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class FakeInternalUserProvider implements UserDAO {
+
+    final Map<String, UserInterno> knownUsers = new HashMap<>();
+
+    public FakeInternalUserProvider() {
+        BasicPasswordEncryptor encryptor = new BasicPasswordEncryptor();
+
+        // Usuario de prueba - gestormunicipal1
+        UserInterno gestormunicipal1 = new UserInterno();
+        gestormunicipal1.setUsername("gestormunicipal1");
+        gestormunicipal1.setPassword(encryptor.encryptPassword("gestormunicipal1"));
+        knownUsers.put("gestormunicipal1", gestormunicipal1);
+    }
+
+    @Override
+    public UserInterno loadUserByUsername(String username, String password) {
+        UserInterno user = knownUsers.get(username.trim());
+        if (user == null) {
+            return null;
+        }
+        BasicPasswordEncryptor encryptor = new BasicPasswordEncryptor();
+        if (encryptor.checkPassword(password, user.getPassword())) {
+            return user;
+        } else {
+            return null;
+        }
+
+    }
+
+    @Override
+    public Collection<UserInterno> listAllUsers() {
+        return knownUsers.values();
+    }
+}
